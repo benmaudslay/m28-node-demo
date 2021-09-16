cc5c52999ad961f3b42cb134ea1c634929b34280
Benjamin Maudslay <benjaminmaudslay@Benjamins-MacBook-Pro-1030.local>
2021-09-16 11:42:58 +0100

    init

---
+++ app.js
@@ -0,0 +1,9 @@
+let { add, multiply } = require("./cleverFunctions");
+// let cleverFunctions = require("./cleverFunctions")
+
+console.log(multiply(2, 3));
+// console.log(cleverFunctions.add(5, 5))
+
+let data = require("./data.json");
+
+console.log(data);
